abc0dc582ac91df02036da53e13a5ae29513f0cd
dannitay <dannielletaylor@outlook.com>
2021-04-17 23:45:31 -0500

    adding reply functionality

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -23,6 +23,34 @@ const commentController = {
 
     },
 
+    //add replies
+    addReply({params, body}, res) {
+      Comment.findOneAndUpdate(
+        { _id: params.commentId },
+        { $push: { replies: body }},
+        { new: true }
+      )
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!'});
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.json(err));
+    },
+
+    //remove replies
+    removeReply({ params }, res) {
+      Comment.findOneAndUpdate(
+        { _id: params.commentId },
+        { $pull: { replies: { replyId: params.replyId }}},
+        { new: true }
+        )
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => res.json(err));
+    },
+
     //remove comment
     removeComment({ params }, res) {
         Comment.findOneAndDelete({ _id: params.commentId })
